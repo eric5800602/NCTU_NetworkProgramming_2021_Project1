5d4c6dff87aac16874433a92dafad8b85bd988da
SymbolWu <symbolwu.cs10@nycu.edu.tw>
2021-10-12 21:43:46 +0800

    [CODE]Add feature Setenv and Printenv

--- npshell.cpp
+++ npshell.cpp
@@ -8,12 +8,58 @@
 
 using namespace std;
 
+void SETENV(string name,string val){
+	setenv(name.c_str(),val.c_str(),1);
+}
+
+void PRINTENV(string name){
+	char *val = getenv(name.c_str());
+	if(val) cout << val << endl;
+}
+
+int EXECCMD(string input){
+	vector<string> parm;
+	string cmd;
+	istringstream iss(input);
+	while(getline(iss,cmd,' ')){
+		parm.push_back(cmd);
+	}
+	if(parm[0] == "setenv"){
+		SETENV(parm[1],parm[2]);
+		return 0;
+	}
+	else if(parm[0] == "printenv"){
+		PRINTENV(parm[1]);
+		return 0;
+	}
+	else if(parm[0] == "exit"){
+		exit(0);
+	}
+	const char **argv = new const char* [parm.size()+1];
+	for(int i=0;i < parm.size();++i){
+		argv[i] = parm[i].c_str();
+	}
+	argv[parm.size()] = NULL;
+	pid_t cpid;
+	int status;
+	cpid = fork();
+	if(cpid != 0){
+		waitpid(cpid,&status,0);
+	}
+	else{
+		cmd = parm[0];
+		if(execvp(cmd.c_str(),(char **)argv) == -1){
+			return -1;
+		}
+	}
+	return 0;
+}
+
 int main(){
 	clearenv();
-	setenv("PATH","bin:.",1);
+	SETENV("PATH","bin:.");
 	while(1){
-		vector<string> parm;
-		string input,cmd;
+		string input;
 		cout << "% ";
 		getline(cin,input);
 		if(!cin)
@@ -22,32 +68,7 @@ int main(){
 				cout << endl;
 				return 0;
 			}
-		istringstream iss(input);
-		while(getline(iss,cmd,' ')){
-			if(cmd == "exit"){
-				exit(0);
-			}else{
-				parm.push_back(cmd);
-			}
-			
-		}
-		const char **argv = new const char* [parm.size()+2];
-		for(int i=0;i < parm.size()+1;++i){
-			argv[i] = parm[i].c_str();
-		}
-		argv[parm.size()+1] = NULL;
-		pid_t cpid;
-		int status;
-	    	cpid = fork();
-		if(cpid != 0){
-			waitpid(cpid,&status,0);
-		}
-		else{
-			cmd = parm[0];
-			if(execvp(cmd.c_str(),(char **)argv) == -1){
-				exit(0);
-			}
-		}
+		if(EXECCMD(input)==-1) return 0;
 	}	
 	return 0;
 }
